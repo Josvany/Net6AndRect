0ceae34b5c30ea2a6279254dc422aee7f3c88fdd
Josvany_G <josvanyg@gmail.com>
2022-06-04 12:31:06 -0600

    react route

--- reactweb/src/main/App.tsx
+++ reactweb/src/main/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HouseList from '../house/HouseList';
 import './App.css';
 import Header from './Header';
 
 function App() {
   return (
-    <div className='container'>
-      <Header subtitle='test'></Header>
-      <HouseList></HouseList>
-    </div>
+    <BrowserRouter>
+      <div className='container'>
+        <Header subtitle='test'></Header>
+        <Routes>
+          <Route path="/" element={<HouseList/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
